224722b31c272664365faf4637de2c0fe292797b
Chirrenthen <chirrenthen13@gmail.com>
2025-03-09 16:28:03 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "calendar-app-v1";
+const ASSETS = [
+  "index.html",
+  "style.css",
+  "script.js",
+  "manifest.json",
+  "calendar.png",
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
+  "https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
